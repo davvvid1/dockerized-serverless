ae17868e1cd2efab11637abfb455ad17baaf5103
Dawid Zegar <davvvid1@gmail.com>
2018-10-18 12:23:59 +0200

    Put and get data from AWS dynamoDB

--- app/index.js
+++ app/index.js
@@ -1,4 +1,45 @@
-module.exports.handler = function (event, context, callback) {
+const AWS = require('aws-sdk');
+
+module.exports.putDcument = function (event, context, callback) {
   // console.log(event);
-  callback(null, {statusCode: 200, body: 'My response888!!!'})
+  const dynamoDb = new AWS.DynamoDB.DocumentClient({
+    api_version: '2012-08-10',
+    region: 'us-east-1'
+  });
+  return dynamoDb.put({
+    TableName: 'serverless-table',
+    Item: {propertyA: 'third'}
+  }).promise()
+    .then(result => {
+      console.log(result);
+      callback(null, { statusCode: 200, body: 'My response888!!!' })
+    })
+    .catch(error => {
+      console.log(error);
+      callback(null, { statusCode: 500, body: 'Error' })
+    })
+};
+
+module.exports.queryDocuments = function (event, context, callback) {
+  // console.log(event);
+  const dynamoDb = new AWS.DynamoDB.DocumentClient({
+    api_version: '2012-08-10',
+    region: 'us-east-1'
+  });
+  dynamoDb.query({
+    TableName: 'serverless-table',
+    KeyConditionExpression: "propertyA = :propertyA",
+    ExpressionAttributeValues: {
+      ":propertyA": 'haha'
+    }
+  }).promise()
+    .then(result => {
+      console.log('DAWID TEST');
+      console.log(result);
+      callback(null, { statusCode: 200, body: JSON.stringify(result) }) // without stringify it gives 500 without reason in logs!
+    })
+    .catch(error => {
+      console.log(error);
+      callback(null, { statusCode: 500, body: 'Error' })
+    })
 };
